21c3c8567afcf190bc87730ce10544e936232969
lucasalustiano <lucas.salustiano@ccc.ufcg.edu.br>
2022-01-10 17:22:01 -0300

    add form validation and state management

--- src/components/FeedbackForm/FeedbackForm.jsx
+++ src/components/FeedbackForm/FeedbackForm.jsx
@@ -5,8 +5,21 @@ import Button from '../Button/Button';
 
 function FeedbackForm() {
   const [text, setText] = useState('')
+  const [btnDisabled, setBtnDisabled] = useState(true)
+  const [message, setMessage] = useState('')
 
   const handleTextChange = (e) => {
+    if (text === '') {
+      setBtnDisabled(true)
+      setMessage(null)
+    } else if (text !== '' && text.trim().length < 10) {
+      setBtnDisabled(true)
+      setMessage(null)
+    } else {
+      setBtnDisabled(false)
+      setBtnDisabled(null)
+    }
+
     setText(e.target.value)
   }
 
@@ -21,8 +34,9 @@ function FeedbackForm() {
             placeholder='Write a review'
             value={text}
           />
-          <Button type='submit'>Send</Button>
+          <Button type='submit' isDisabled={btnDisabled}>Send</Button>
         </div>
+        {message && <div className='message'>{message}</div>}
       </form>
     </Card>
   );
